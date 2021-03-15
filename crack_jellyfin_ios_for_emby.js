27e09c4d481d81d9e69c7e46ddd1345a04bc0166
Eric Chan <tces1@hotmail.com>
2021-03-15 11:12:19 +0800

    Update crack_jellyfin_ios_for_emby.js

--- crack_jellyfin_ios_for_emby.js
+++ crack_jellyfin_ios_for_emby.js
@@ -1,18 +1,12 @@
-$notification.post("伪装Jellyfin服务器开始", "", "");
-if ($request.url.indexOf('/system/info/public') != -1) {
-    if($response.status==200){
-        $notification.post(“debug”, $response.body, $response.body);
-        var $tmp_body = $response.body
-        if($response.body.ProductName != "Jellyfin Server"){
-            $notification.post("伪装Jellyfin服务器成功", "", "");
-            $tmp_body["ProductName"] = "Jellyfin Server"
-            $done({status: 200, headers: $response.headers, body: $tmp_body })
-        }else{
-            $done({})
-        }
-    }else{
-        $done({})
-    }
-}else{
-    $done({})
+let body = $response.body;
+if($request.url.includes("/system/info/public"))
+{
+    body = 
+    `
+    {"ServerName":"crack_jellyfin_ios_for_emby","ProductName":"Jellyfin Server","OperatingSystem":"Linux","StartupWizardCompleted":true}
+    `
 }
+$done({
+    status : 200,
+    body : body
+});
